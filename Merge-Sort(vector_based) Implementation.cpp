12ea78210b6b698c4dee1fd0799148a0cab0bcb5
SHEEN <161808007+Normenghub@users.noreply.github.com>
2025-12-07 20:03:53 +0900

    Implement vector-based Merge Sort algorithm

---
+++ Merge-Sort(vector_based) Implementation.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+
+template<typename E, typename C>
+void merge(vector<E>& in, vector<E>& out, const C& less, int b, int m){
+    int i = b;
+    int j = b + m;
+    int n = in.size();
+    int e1 = min(b+m,n);
+    int e2 = min(b + 2*m, n);
+    int k = b;
+    while((i < e1) && (j < e2)){
+        if(!less(in[j], in[i])) out[k++] = in[i++];
+        else out[k++] = in[j++];
+    }
+    while(i < e1) out[k++] = in[i++];
+    while(i < e2) out[k++] = in[j++];
+}
+
+template<typename E, typename C>
+void mergeSort(vector<E>& S, const C& less){
+    typedef vector<E> vect;
+    int n = S.size();
+    vect v1(S); vect* in = &v1;
+    vect v2(n); vect* out = &v2;
+    for(int m = 1; m < n; m *=2){
+        for(int b= 0; b< n; b+= 2*m){
+            merge(*in, *out, less,b,m);
+        }
+        swap(in,out);
+    }
+    S= *in;
+}
